7174e4f3f1ab93791b870b0db08dff6d0e0b577c
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-02-11 22:22:35 -0500

    comment create, update, and delete work!

--- src/Components/Comments/updateComment.tsx
+++ src/Components/Comments/updateComment.tsx
@@ -1,21 +1,54 @@
 import React from "react";
 import Radium from "radium";
+import { Comments } from './commentTable';
+import {Form, FormGroup, Input, Button, Label} from 'reactstrap';
 
-class UpdateComment extends React.Component <{},any> {
-    constructor(props:any){
+type Props = {
+    token: string,
+    commentToUpdate: Comments,
+    updateOff: () => void,
+    fetch: () => void,
+}
+
+type State = {
+    content: string
+}
+
+class UpdateComment extends React.Component<Props, any> {
+    constructor(props: any) {
         super(props)
+        this.state = {
+            content: ''
+        }
     }
 
-    componentDidMount=()=>{
+    componentDidMount = () => {
 
     }
 
-    render(){
-        return(
-            <div>
-                {/* modal */}
-                update comment
-            </div>
+    commentUpdate = () => {
+        fetch(`http://localhost:3000/comment/update/${this.props.commentToUpdate.id}`, {
+            method: 'PUT',
+            body: JSON.stringify({comment: {content: this.state.content, postId: this.props.commentToUpdate.postId}}),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${this.props.token}`
+            })
+        }) .then((res) => {
+            this.props.fetch();
+            this.props.updateOff();
+        })
+    }
+
+    render() {
+        return (
+            <Form inline onSubmit={e => {e.preventDefault(); this.commentUpdate() }}>
+            <FormGroup>
+                <Label>update Comment</Label>
+                <Input value={this.state.content} onChange={(e) => this.setState({content: e.target.value})}/>
+            </FormGroup>
+            <Button type="submit">Comment</Button>
+        </Form>
         )
     }
 }
