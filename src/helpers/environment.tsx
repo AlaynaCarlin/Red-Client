c5af151e780bbca38f910c0b0811a2c00b586e83
AlaynaCarlin <alaynacarlin18@gmail.com>
2022-02-15 11:48:37 -0500

    fixing?

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'amc-class-and-sass-client.herokuapp.com/':
+    case 'amc-class-and-sass-client.herokuapp.com':
         APIURL = 'https://amc-class-and-sass-server.herokuapp.com'
 }
 
